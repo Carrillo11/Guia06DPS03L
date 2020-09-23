cc495986fae03d06a2c3947fbdc4ac39a151023b
Carrillo11 <renecarrillo120@gmail.com>
2020-09-22 20:55:20 -0600

    Se han modificado distintos componentes y se ha agregado el inicio de sesion con fb y tw

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
-};
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBuFv73ZA-zo-Z3Ly0nQi6tQrDjDzzB9WQ",
+    authDomain: "authangularfirebase-8f9fa.firebaseapp.com",
+    databaseURL: "https://authangularfirebase-8f9fa.firebaseio.com",
+    projectId: "authangularfirebase-8f9fa",
+    storageBucket: "authangularfirebase-8f9fa.appspot.com",
+    messagingSenderId: "1005436690162",
+    appId: "1:1005436690162:web:5e4f9c493ea9b046db3a6b",
+    measurementId: "G-P2NSSN4QTZ"
+  }
+  };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
-
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBuFv73ZA-zo-Z3Ly0nQi6tQrDjDzzB9WQ",
+    authDomain: "authangularfirebase-8f9fa.firebaseapp.com",
+    databaseURL: "https://authangularfirebase-8f9fa.firebaseio.com",
+    projectId: "authangularfirebase-8f9fa",
+    storageBucket: "authangularfirebase-8f9fa.appspot.com",
+    messagingSenderId: "1005436690162",
+    appId: "1:1005436690162:web:5e4f9c493ea9b046db3a6b",
+    measurementId: "G-P2NSSN4QTZ"
+  }
+  };
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
